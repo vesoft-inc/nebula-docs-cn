7508a52fe31b6354247681f3f5f7d66dcc1021dd
randomJoe211 <69501902+randomJoe211@users.noreply.github.com>
2023-03-17 15:38:05 +0800

    update count token

--- count_token_module.py
+++ count_token_module.py
@@ -21,16 +21,24 @@ def count_tokens_in_files(root_dir, encoding):
                 token_counts.append((file_path, num_tokens))
     return token_counts
 
-def write_token_counts_to_file(token_counts, output_dir):
+def write_token_counts_to_file(token_counts, output_dir, max_token_count=1500):
     output_file = os.path.join(output_dir, 'token_counts.txt')
+    to_be_split = []
+
     with open(output_file, 'w', encoding='utf-8') as f:
         for file_path, num_tokens in token_counts:
             f.write(f"{file_path}: {num_tokens} tokens\n")
+            if num_tokens > max_token_count:
+                to_be_split.append(file_path)
 
-def process_directory(output_dir, encoding_name='cl100k_base'):
+        f.write("\nFiles to be split:\n")
+        for file_path in to_be_split:
+            f.write(f"{file_path}\n")
+
+def process_directory(output_dir, encoding_name='cl100k_base', max_token_count=1500):
     encoding = get_encoding(encoding_name)
     token_counts = count_tokens_in_files(output_dir, encoding)
-    write_token_counts_to_file(token_counts, output_dir)
+    write_token_counts_to_file(token_counts, output_dir, max_token_count)
 
 # Example usage:
 output_dir = 'output'
