686d48e7a0fb6b74b9972bd4d05817003d62bfde
randomJoe211 <69501902+randomJoe211@users.noreply.github.com>
2023-03-30 17:42:37 +0800

    Update generate_dataset_module.py

--- scripts/fine-tuning/generate_dataset_module.py
+++ scripts/fine-tuning/generate_dataset_module.py
@@ -11,7 +11,7 @@ def generate_prompt_completion_dataset(output, gpt3_api_key, model, max_tokens, 
     files = os.listdir(output)
     if len(files) == 0:
         raise ValueError(f"Output directory '{output}' is empty.")
-    files = sorted([os.path.join(output, f) for f in files])
+    files = sorted([os.path.join(output, f) for f in files if f.endswith('.md')])
 
     # Send each chunk of text to GPT-3.5 to generate prompt completions
     completions = []
@@ -33,6 +33,10 @@ def generate_prompt_completion_dataset(output, gpt3_api_key, model, max_tokens, 
             completions.append((text.strip(), response.choices[0].text.strip()))
         else:
             completions.append((text.strip(), ""))
+    
+    # Create the dataset directory if it doesn't exist
+    if not os.path.exists(dataset_dir):
+        os.makedirs(dataset_dir)
     
     # Write the prompt-completion pairs to a file in JSONL format
     dataset_file = os.path.join(dataset_dir, 'prompt_completion_dataset.jsonl')
