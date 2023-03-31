fcbddbf81b1a6ad2fde951cd5de4a83d9373ab94
randomJoe211 <69501902+randomJoe211@users.noreply.github.com>
2023-03-31 11:38:12 +0800

    incremental request

--- scripts/fine-tuning/generate_dataset_module.py
+++ scripts/fine-tuning/generate_dataset_module.py
@@ -5,38 +5,12 @@ def generate_prompt_completion_dataset(output, gpt3_api_key, model, max_tokens, 
     # Load API key and model
     openai.api_key = gpt3_api_key
 
-# Load the split Markdown files from the output directory
-    if not os.path.exists(output):
-        raise ValueError(f"Output directory '{output}' does not exist.")
+    # Load the Markdown files from the output directory and its subdirectories
     files = []
     for root, _, filenames in os.walk(output):
         for filename in filenames:
             if filename.endswith('.md'):
                 files.append(os.path.join(root, filename))
-    if len(files) == 0:
-        raise ValueError(f"No Markdown files found in directory '{output}' or its subdirectories.")
-    files = sorted(files)
-
-    # Send each chunk of text to GPT-3.5 to generate prompt completions
-    completions = []
-    for file in files:
-        with open(file, 'r', encoding='utf-8') as f:
-            text = f.read()
-        prompt = "Transform the following content to prompt-completion so that we can fine-tune GPT-3 with it. "
-        response = openai.Completion.create(
-            engine=model,
-            prompt=prompt + text,
-            max_tokens=max_tokens,
-            n=1,
-            temperature=temperature,
-            stop=None,
-            frequency_penalty=0,
-            presence_penalty=0
-        )
-        if len(response.choices) > 0:
-            completions.append((text.strip(), response.choices[0].text.strip()))
-        else:
-            completions.append((text.strip(), ""))
 
     # Create the dataset directory if it doesn't exist
     if not os.path.exists(dataset_dir):
@@ -45,8 +19,35 @@ def generate_prompt_completion_dataset(output, gpt3_api_key, model, max_tokens, 
     # Write the prompt-completion pairs to a file in JSONL format
     dataset_file = os.path.join(dataset_dir, 'prompt_completion_dataset.jsonl')
     with open(dataset_file, 'w', encoding='utf-8') as f:
-        for prompt, completion in completions:
-            f.write(f'{{"prompt": "{prompt}", "completion": "{completion}"}}\n')
+        # Send each Markdown file to OpenAI's API to generate prompt completions
+        for file in files:
+            with open(file, 'r', encoding='utf-8') as md_file:
+                # Read the contents of the Markdown file
+                text = md_file.read()
+                
+                # Generate the prompt from the contents of the Markdown file
+                prompt = "Transform the following content to prompt-completion so that we can fine-tune GPT-3 with it. "
+                
+                # Send the prompt to OpenAI's API to generate a completion
+                response = openai.Completion.create(
+                    engine=model,
+                    prompt=prompt + text,
+                    max_tokens=max_tokens,
+                    n=1,
+                    temperature=temperature,
+                    stop=None,
+                    frequency_penalty=0,
+                    presence_penalty=0
+                )
+                
+                # Extract the completion text from the response
+                if len(response.choices) > 0:
+                    completion = response.choices[0].text.strip()
+                else:
+                    completion = ""
+                
+                # Write the prompt-completion pair to the output file
+                f.write(f'{{"prompt": "{text.strip()}", "completion": "{completion}"}}\n')
     
     # Return the path to the generated prompt-completion dataset file
     return dataset_file
