d25f9035e76c49421bb2c3a52e6633ef3b912aae
min.wu <50101159+whitewum@users.noreply.github.com>
2023-05-08 17:49:05 +0800

    Update conditional_yml.py

--- scripts/conditional_yml.py
+++ scripts/conditional_yml.py
@@ -57,6 +57,7 @@ if __name__ == '__main__':
     with open(mkdocs_yml_path, 'r', encoding='utf-8') as f:
         config = yaml.safe_load(f)
     database_edition = config.get("extra", {}).get("database_edition", "")
+    print(database_edition)
     if database_edition not in ['community', 'enterprise', 'both']:
         raise ValueError("Invalid value for database_edition: {}".format(database_edition))
     process_mkdocs_yml(mkdocs_yml_path, database_edition)
