e10e5d26315a63e77737cad4751dca362746ed55
min.wu <50101159+whitewum@users.noreply.github.com>
2023-05-08 18:43:15 +0800

    Update conditional_render.py

--- scripts/conditional_render.py
+++ scripts/conditional_render.py
@@ -34,10 +34,10 @@ def process_files(file_path, database_edition):
                     f.write(content)
 
 if __name__ == '__main__':
-    mkdocs_yml_path = 'mkdocs.yml'
-    with open(mkdocs_yml_path, 'r', encoding='utf-8') as f:
+    yml_path = 'database_conedition.yml'
+    with open(yml_path, 'r', encoding='utf-8') as f:
         config = yaml.safe_load(f)
-    database_edition = config.get("extra", {}).get("database_edition", "")
+    database_edition = config.get("database_edition", "")
     if database_edition not in ['community', 'enterprise', 'both']:
         raise ValueError("Invalid value for database_edition: {}".format(database_edition))
     file_path = 'docs-2.0/'
